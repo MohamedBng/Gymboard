aabd62daaebbb9617ea395ef7f2cfbf49528cf82
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-11-01 20:11:20 +0100

    add association on muscle group ans training session model

--- app/models/muscle_group.rb
+++ app/models/muscle_group.rb
@@ -3,6 +3,8 @@ class MuscleGroup < ApplicationRecord
 
   has_many :muscles, dependent: :destroy
   has_many :exercises, dependent: :destroy
+  has_many :training_session_muscle_groups, dependent: :destroy
+  has_many :training_sessions, through: :training_session_muscle_groups
 
   def self.ransackable_attributes(auth_object = nil)
     %w[name]
