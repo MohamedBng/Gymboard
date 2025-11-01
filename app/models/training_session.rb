573fdaacad8861a82288b04b165105cfc81d7515
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-11-01 19:37:08 +0100

    add model and validations

---
+++ app/models/training_session.rb
@@ -0,0 +1,24 @@
+class TrainingSession < ApplicationRecord
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validate :end_time_after_start_time
+
+  before_validation :set_default_name, if: -> { name.blank? }
+
+  private
+
+  def end_time_after_start_time
+    return unless start_time && end_time
+
+    if end_time <= start_time
+      errors.add(:end_time, :must_be_after_start_time)
+    end
+  end
+
+  def set_default_name
+    return unless start_time
+
+    date_format = start_time.strftime("%B %d")
+    self.name = "Session — #{date_format}"
+  end
+end
