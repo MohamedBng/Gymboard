b88424668462d1cbf7184502fbad39b5aab9558d
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-11-01 13:15:29 +0100

    create table muscle_group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_20_160806) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_01_120541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -67,6 +67,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_20_160806) do
     t.integer "muscle_group", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "muscle_groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_muscle_groups_on_name", unique: true
   end
 
   create_table "muscles", force: :cascade do |t|
