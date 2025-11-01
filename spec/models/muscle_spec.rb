ad9da854d52e1cf4bbab9b7cb20cb36868344cdc
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2025-11-01 14:04:21 +0100

    test that muscle belongs to muscle group

--- spec/models/muscle_spec.rb
+++ spec/models/muscle_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe Muscle, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:muscle_group) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:muscle_group) }
   end
 end
