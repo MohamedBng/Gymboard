51385483eb6bda8aa81247e98c0a714bd511d6f7
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-01-18 18:16:33 +0100

    remove enum role and is validation on model exercise_muscle

--- app/models/exercise_muscle.rb
+++ app/models/exercise_muscle.rb
@@ -1,11 +1,4 @@
 class ExerciseMuscle < ApplicationRecord
   belongs_to :exercise
   belongs_to :muscle
-
-  enum :role, [
-    :primary,
-    :secondary
-  ]
-
-  validates :role, presence: true
 end
