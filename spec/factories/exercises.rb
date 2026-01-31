e2a75063a4fdd4cb9a00eafb7befd99903e7a1dd
Mohamed Bengrich <mohamed.bengrich@outlook.fr>
2026-01-31 16:14:39 +0100

    update exercise factory

--- spec/factories/exercises.rb
+++ spec/factories/exercises.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :exercise do
     sequence(:title) { |n| "#{Faker::Sports::Basketball.player} #{n}" }
     association :muscle_group
-    primary_muscle { association(:muscle) }
+    association :primary_muscle, factory: :muscle
+
+    trait :with_secondary_muscles do
+      after(:create) do |exercise|
+        exercise.secondary_muscles << create_list(:muscle, 2)
+      end
+    end
   end
 end
